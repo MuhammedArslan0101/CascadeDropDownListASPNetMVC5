9059efc50a8cf217ef3c925196e03be94a61d3cb
Muhammed ARSLAN <=md.arslan0101@gmail.com>
2021-06-06 15:39:44 +0300

    Crud Country State City

---
+++ CascadeDropDownListASPNetMVC5/Controllers/CitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CascadeDropDownListASPNetMVC5.Models;
+
+namespace CascadeDropDownListASPNetMVC5.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Cities
+        public ActionResult Index()
+        {
+            var citys = db.Citys.Include(c => c.State);
+            return View(citys.ToList());
+        }
+
+        // GET: Cities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Citys.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // GET: Cities/Create
+        public ActionResult Create()
+        {
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName");
+            return View();
+        }
+
+        // POST: Cities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CityId,CityName,StateId")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Citys.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName", city.StateId);
+            return View(city);
+        }
+
+        // GET: Cities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Citys.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName", city.StateId);
+            return View(city);
+        }
+
+        // POST: Cities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CityId,CityName,StateId")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName", city.StateId);
+            return View(city);
+        }
+
+        // GET: Cities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Citys.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            City city = db.Citys.Find(id);
+            db.Citys.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
